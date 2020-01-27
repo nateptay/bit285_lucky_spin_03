24b4db5e3a9707925f4a477519eb96b7555248cd
moscowkids <robtay@gmail.com>
2020-01-27 15:31:33 -0800

    Step 4 Completion

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -10,18 +10,19 @@ namespace LuckySpin.Controllers
         //The Conroller to receive the Luck number from the Route
         public IActionResult Index(int luck = 7) //Default value for luck is 7
         {
+            LuckySpin.Models.Spin spin = new LuckySpin.Models.Spin();
             int a = random.Next(1, 10);
             int b = random.Next(1, 10);
             int c = random.Next(1, 10);
 
             // Load up the ViewBag for use by the Spinner View "Index.cshtml"
-            ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
-            ViewBag.A = a;
-            ViewBag.B = b;
-            ViewBag.C = c;
-            ViewBag.luck = luck;
+            spin.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
+            spin.SpinNum1 = a;
+            spin.SpinNum2 = b;
+            spin.SpinNum3 = c;
+            spin.LuckyNumber = luck;
 
-            return View();
+            return View(spin);
         }
     }
 }
---
+++ LuckySpin/Models/Spin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class Spin
+    {
+        public int LuckyNumber { get; set; }
+        public int SpinNum1 { get; set; }
+        public int SpinNum2 { get; set; }
+        public int SpinNum3 { get; set; }
+        public string ImgDisplay { get; set; }
+
+    }
+}
